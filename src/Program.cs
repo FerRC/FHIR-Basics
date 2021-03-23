a0c9f367be188fa85149efcb958c1ee5b8a290c6
Fernando Ramos <fernando.ramos@credibleinc.com>
2021-03-23 16:18:15 -0600

    Gettting Patients and Encounters of the Patient

--- src/Program.cs
+++ src/Program.cs
@@ -21,15 +21,34 @@ namespace FHIR_Basics
             FhirClient client = new FhirClient(server, settings);
 
             Bundle patientsBundle = client.Search<Patient>(null);
-            Console.WriteLine($"Total of Patients:{patientsBundle.Total}");
+            Console.WriteLine($"Total of Patients:{patientsBundle.Total} and the Entry Count is:{patientsBundle.Entry.Count}");
 
-            foreach (Bundle.EntryComponent entry in patientsBundle.Entry)
+            foreach (Bundle.EntryComponent patientEntry in patientsBundle.Entry)
             {
 
-                if (entry != null)
+                if (patientEntry.Resource != null)
                 {
-                    Patient patient = (Patient)entry.Resource;
-                    Console.WriteLine($"The Patient name is {patient.Name}");
+                    Patient patient = (Patient)patientEntry.Resource;
+                    if (patient.Name.Count > 0)
+                    {
+                        Console.WriteLine($"The Patient name is {patient.Name[0]}");
+                    }
+
+                    Bundle encounterBundle = client.Search<Encounter>(
+                        new string[]
+                        {
+                            $"patient=Patient/{patient.Id}"
+                        });
+
+                    Console.WriteLine($"The total encounters are:{encounterBundle.Total} and the Entry count is:{encounterBundle.Entry.Count}");
+                    foreach (Bundle.EntryComponent encounterEntry in encounterBundle.Entry)
+                    {
+                        if (encounterEntry.Resource != null)
+                        {
+                            Encounter encounter = (Encounter)encounterEntry.Resource;
+                            Console.WriteLine($"\tThe Encounter number is:{encounter.Id} and the date is:{encounter.Period.Start}");
+                        }
+                    }
                 }
             }
         }
